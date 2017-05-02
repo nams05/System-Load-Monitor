f98c03f0c3945f375509d5fd97574b6f46e15693
Namrata Gupta <namrata.gupta05@gmail.com>
2017-05-02 20:24:56 +0530

    added comments

--- send_report.py
+++ send_report.py
@@ -91,10 +91,10 @@ def binary_search(key,start,end):
 			return None
 
 def calculate_average(column_index_list, timestamp_start, timestamp_end): # avg of the interval timestamp_end - timestamp_start
-	#intialize local variable
-	start=time.time()
 	global total_lines
 	global line_offset
+	#intialize local variable
+	start=time.time()
 	attribute_averages=[0 for i in range(len(column_index_list))]
 	count=0
 	try:
@@ -105,13 +105,13 @@ def calculate_average(column_index_list, timestamp_start, timestamp_end): # avg 
 				index=1
 			file.seek(line_offset[index-1])
 			for line in file:
-				modified_line=line.rstrip('\n')
-				new_tuple=ast.literal_eval(modified_line)
+				modified_line=line.rstrip('\n') 
+				new_tuple=ast.literal_eval(modified_line)#convert read line string to list/tuple according to syntax
 				if (new_tuple[0])>timestamp_end :
 					break
 				elif (new_tuple[0]>=timestamp_start):
 					for i in range(len(column_index_list)):
-						if column_index_list[i]==12:
+						if column_index_list[i]==column_index['users']: # can't calculate avg for strings 
 							continue
 						attribute_averages[i]+=new_tuple[column_index_list[i]]
 					count+=1
@@ -119,10 +119,10 @@ def calculate_average(column_index_list, timestamp_start, timestamp_end): # avg 
 		logger.exception(str(e))
 		quit()
 
-	if count==0:
+	if count==0: #means no entry for the interval found ,so return 0 values for all attributes
 		return attribute_averages
 	for i in range(len(column_index_list)):
-		if column_index_list[i]==12:
+		if column_index_list[i]==column_index['users']: 
 			attribute_averages[i]=get_unique_users(timestamp_start,timestamp_end)
 			continue
 		attribute_averages[i]=attribute_averages[i]/count
@@ -132,7 +132,7 @@ def calculate_average(column_index_list, timestamp_start, timestamp_end): # avg 
 	logger.info('Time taken to execute calculate_average(): %s secs' %"{0:.2f}".format(stop-start))
 	return attribute_averages
 
-def get_unique_users(timestamp_start, timestamp_end):
+def get_unique_users(timestamp_start, timestamp_end): #find all new users for interval timestamp_end - timestamp_start
 	unique_users=set([])
 	global total_lines
 	global line_offset
@@ -158,8 +158,7 @@ def get_unique_users(timestamp_start, timestamp_end):
 
 	return users.rstrip(",") #remove the trailing comma
 
-#produce html code for email body
-def generate_report_table_html(column_index_list,timestamp_start, timestamp_end,total_attachments):
+def generate_report_table_html(column_index_list,timestamp_start, timestamp_end,total_attachments): #produce html code for email body
 	#current time
 	now=time.time()
 	tm_struct=time.localtime(now)
@@ -209,11 +208,11 @@ def generate_report_table_html(column_index_list,timestamp_start, timestamp_end,
 	</head>
 	<body>'''
 	html+="Mail triggered at: "+ date+" "+mail_time+"<br>Hostname: "+ get_hostname()+"<br>Public IP: "+get_public_ip()
-	if column_index_list:
+	if column_index_list: # executed if table flag is on ie column_index_list is not empty in config.ini
 		html+='''<br>Following table contains various system metrics:<br><br>
 				<table><tr><th>Time (hours)</th>'''
 		for i in column_index_list:
-			html+="<th>"+graph_label_index[i]+"</th>"
+			html+="<th>"+graph_label_index[i]+"</th>" # headings of the table are mapped to column no.s in graph_label_index  
 		html+="</tr>"	
 		for i in range(0,24):
 			if(timestamp_of_each_hour[i]>timestamp_end):
@@ -232,16 +231,16 @@ def generate_report_table_html(column_index_list,timestamp_start, timestamp_end,
 					hour_timestamp_end=timestamp_end
 			averages=(calculate_average(column_index_list, hour_timestamp_start, hour_timestamp_end))
 
-			if (averages[0]==0):
+			if (averages[0]==0): #skip the row if no data is found for that hour
 				continue
 			html+="<tr>"
-			html+="<td style=\"background-color:#7C7474\">"+str(i)+"</td>"
+			html+="<td style=\"background-color:#7C7474\">"+str(i)+"</td>" #column 1 of table :time (hours)
 			for j in averages:
 				html+="<td style=\"background-color:"+color(j)+"\">"+str(j)+"</td>"
 
 			html+="</tr>"
 		html+="</table><br><br>"
-	for i in range(total_attachments):
+	for i in range(total_attachments): # in case there are any graphs we need to attach
 		if (i+1)&1:
 			html+="<img style=\"float:left\" src=\"cid:image"+str(i+1)+"\">"
 		else:
@@ -253,6 +252,7 @@ def generate_report_table_html(column_index_list,timestamp_start, timestamp_end,
 	return html
 
 def plot_graph(list_of_column_index_list,timestamp_start,timestamp_end,axis_label_list):
+	"""This function takes a nested list of column_index and plots 1 graph for each list with axis labels defined in axis_label_list"""
 	plot_time_start=time.time()
 	global column_index
 	global graph_label_index
@@ -285,7 +285,7 @@ def plot_graph(list_of_column_index_list,timestamp_start,timestamp_end,axis_labe
 
 	## plot graph using input_list
 	graphline_color={0:'r',1:'g',2:'b',3:'m'} # color of the graph lines
-	for i in range(len(list_of_column_index_list)):
+	for i in range(len(list_of_column_index_list)): #1 graph for 1 list
 		plt.close('all')
 		#set size of the graph image
 		fig_size = plt.rcParams["figure.figsize"]
@@ -301,7 +301,7 @@ def plot_graph(list_of_column_index_list,timestamp_start,timestamp_end,axis_labe
 			plt.setp(graph_name[j],color=graphline_color[j], linewidth=1.0)
 		plt.legend(loc='upper right')
 		plt.xlabel('Timestamp')
-		plt.ylabel(axis_label_list[j])
+		plt.ylabel(axis_label_list[i])
 		# plt.axis([0, 23,0,100],facecolor='b')
 		plt.grid(True)
 		plt.savefig(get_current_directory()+'/graph/'+date+'_'+ str(i+1) +'_graph.jpg')
@@ -314,7 +314,7 @@ def plot_graph(list_of_column_index_list,timestamp_start,timestamp_end,axis_labe
 	plot_time_end=time.time()
 	logger.info('Time taken to plot all graphs: %s secs' %("{0:.2f}".format(plot_time_end-plot_time_start)))
 
-def send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments):
+def send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments): 
 	try:
 		start_time=time.time()
 		dir= get_current_directory()
@@ -326,9 +326,11 @@ def send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments):
 		msg['Subject'] = "Daily Report | Date: " + date +" | Hostname: "+ get_hostname()
 		msg.preamble = "System Load Report"
 
+		# attach html body
 		html_part=MIMEText(html_body,'html')
 		msg.attach(html_part)
 
+		# add header for each graph that has to be attached in email
 		for i in range(1,total_attachments+1):
 			img_data = open(dir+'/graph/'+date+'_'+str(i)+'_graph.jpg', 'rb').read()
 			image= MIMEImage(img_data, name=os.path.basename(dir+'/graph/'+date+'_'+str(i)+'_graph.jpg'))
@@ -340,6 +342,7 @@ def send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments):
 		# body = Lines[0]+ Lines[1]+ Lines[2]+ Lines[3]
 		# f.close()
 		# msg.attach(MIMEText(body, 'plain')) #attach body to MIME msg
+
 		#create SMTP object for connection
 		server = smtplib.SMTP('smtp.gmail.com', 587)
 		server.set_debuglevel(0)
@@ -364,14 +367,16 @@ def send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments):
 		quit()
 
 def main(timestamp_start,timestamp_end,column_index_list,list_of_column_index_list,axis_label_list):
+	# wrapper function 
 	try:
 		dotenv.load(get_current_directory()+'/.env')
 		start_time=time.time()
 		date=get_date_from_timestamp(timestamp_start)
 
-		#plotting all graphs
+		#plot graph
 		plot_graph(list_of_column_index_list, timestamp_start,timestamp_end,axis_label_list)
 		total_attachments=len(list_of_column_index_list)
+
 		# compose the email
 		fromaddr = dotenv.get("From")
 		toaddr = dotenv.get("To")
@@ -379,7 +384,10 @@ def main(timestamp_start,timestamp_end,column_index_list,list_of_column_index_li
 		bcc = dotenv.get("Bcc")
 		rcpt =[cc] + [bcc] + [toaddr]
 		
+		#generate html code for email body
 		html_body =(generate_report_table_html(column_index_list,timestamp_start,timestamp_end,total_attachments))
+
+		#send mail with graphs and table
 		send_mail(date,fromaddr,toaddr,cc,bcc,rcpt,html_body,total_attachments)
 		end_time=time.time()
 		logger.info('Time taken to execute %s(): %s secs' %(inspect.currentframe().f_code.co_name,"{0:.2f}".format(end_time-start_time)))
@@ -398,6 +406,7 @@ if __name__=='__main__':
 		start_time=time.time()
 		check_directory_exists(get_current_directory()+'/log')
 
+		#if no arguments are passed, generate email report for entire day else for the time interval passed
 		if len(sys.argv)>1:
 			timestamp_of_each_hour=get_timestamp_of_each_hour(int(sys.argv[1]))
 			timestamp_start=int(sys.argv[1])
@@ -413,11 +422,12 @@ if __name__=='__main__':
 		logger=logging.getLogger(__name__)
 		logger.setLevel(logging.DEBUG)
 		formatter=logging.Formatter('%(asctime)s [%(levelname)s] %(message)s')
-		file_handler=logging.FileHandler(get_current_directory()+'/log/'+date+'.log')
+		file_handler=logging.FileHandler(get_current_directory()+'/log/'+date+'.log') #new log file for new date
 		file_handler.setFormatter(formatter)
 		logger.addHandler(file_handler)
 		# logging.basicConfig(filename='file.log',level=logging.DEBUG,format='%(asctime)s [%(levelname)s] %(message)s')
 
+		#map attributes to column numbers in the input file
 		column_index={
 			'timestamp':0,
 			'cpu usage':1,
@@ -450,6 +460,7 @@ if __name__=='__main__':
 		12:'Users'
 		}
 
+		#read file to store offsets for each line and count the number of lines
 		with open(get_current_directory()+'/data/'+date+'.txt') as file:
 			line_offset = []
 			offset = 0
@@ -459,6 +470,7 @@ if __name__=='__main__':
 				offset += len(line)
 				total_lines+=1
 
+		##email report is generated according to lists which are sent as arguments. These lists are created according to values read from config.ini 
 		config = ConfigObj('config.ini')
 		column_index_list=[]
 		i=1
@@ -476,7 +488,7 @@ if __name__=='__main__':
 			flag=0
 			for sub_section in config['Graph'][section]:
 				if config['Graph'][section].as_bool(sub_section) ==True:
-					if flag == 0:
+					if flag == 0: # label is added once for each sub-section
 						axis_label_list.append(section)
 						flag=1
 					temp.append(i)
@@ -484,7 +496,6 @@ if __name__=='__main__':
 			if temp:
 				list_of_column_index_list.append(temp)
 			del temp
-		print axis_label_list
 
 		main(timestamp_start,timestamp_end,column_index_list,list_of_column_index_list,axis_label_list)
 		end_time=time.time()
